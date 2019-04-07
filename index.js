5c5bea7dadfbab8b36492d7fd7e3eaffbae2fe11
morriq <dawid.winiarczyk@gmail.com>
2019-04-07 10:50:49 +0200

    New: Add command in npm: start and dev

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const { PORT } = require('./config/app-env');
 
 const app = express();
 
-app.listen(PORT, () => console.log('hello'));
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
